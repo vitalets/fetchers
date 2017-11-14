975d2d4d7431c69e1999d19610c742f8c73da9eb
vitalets <vitalets@yandex-team.ru>
2017-11-14 21:19:39 +0300

    add a few tests for rest-fetcher

--- test/specs/rest-fetcher.test.js
+++ test/specs/rest-fetcher.test.js
@@ -1,13 +1,24 @@
 
-const URL = 'http://test.com';
+const url = 'http://test.com';
+const headers = {
+  Accept: 'application/json'
+};
 
 describe('RestFetcher', function () {
-  beforeEach(function () {
-    this.fetcher = new RestFetcher(URL);
+  before(function () {
+    this.fetcher = new RestFetcher(url, {headers});
   });
   it('should GET json', async function () {
-    fetchMock.getOnce(URL, {hello: 'world'});
+    fetchMock.getOnce(url, {foo: 'bar'});
     const json = await this.fetcher.get().then(r => r.json());
-    assert.deepEqual(json, {hello: 'world'});
+    assert.deepEqual(fetchMock.lastOptions(url), {method: 'GET', body: null, headers});
+    assert.deepEqual(json, {foo: 'bar'});
+  });
+  it('should POST json', async function () {
+    fetchMock.postOnce(url, {foo: 'bar'});
+    const body = JSON.stringify({x: 1});
+    const json = await this.fetcher.post(body).then(r => r.json());
+    assert.deepEqual(fetchMock.lastOptions(url), {method: 'POST', body, headers});
+    assert.deepEqual(json, {foo: 'bar'});
   });
 });
