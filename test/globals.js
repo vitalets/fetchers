da81a7ae5188c32c0a1ba22ece1b1fe095c30538
vitalets <vitalets@yandex-team.ru>
2017-11-15 19:07:23 +0300

    build: targets to es5 es2017

--- test/globals.js
+++ test/globals.js
@@ -3,15 +3,17 @@ import {RestFetcher, PathRestFetcher} from '../src';
 import fetch from 'node-fetch';
 import fetchMock from 'fetch-mock';
 import assert from 'assert';
+import path from 'path';
 
 global.fetch = fetch;
 global.fetchMock = fetchMock;
 global.assert = assert;
 if (process.env.LIB_PATH) {
-  const fetchers = require(process.env.LIB_PATH);
+  const fetchers = require(path.resolve(process.env.LIB_PATH));
   global.RestFetcher = fetchers.RestFetcher;
   global.PathRestFetcher = fetchers.PathRestFetcher;
 } else {
   global.RestFetcher = RestFetcher;
   global.PathRestFetcher = PathRestFetcher;
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,44 @@ const path = require('path');
 const webpack = require('webpack');
 const packageJson = require('./package');
 
-const outDir = process.env.RUNTYPER ? '.runtyper' : 'dist';
-const runtyper = process.env.RUNTYPER ? ['babel-plugin-runtyper', {
-  warnLevel: 'break',
-  implicitAddStringNumber: 'allow',
-}] : null;
-const babelPlugins = [runtyper].filter(Boolean);
-const outFile = path.basename(packageJson.main);
+const target = process.env.TARGET || 'es2017';
+module.exports = createConfig(target);
 
-module.exports = {
-  entry: './src',
-  output: {
-    path: path.resolve(outDir),
-    filename: outFile,
-    libraryTarget: 'umd',
-    library: 'Fetchers',
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-            plugins: babelPlugins,
-          }
-        }
-      }
+function createConfig(target) {
+  const outDir = path.join('dist', target);
+  const outFile = path.basename(packageJson.main);
+  return {
+    entry: [
+      target === 'es5' ? 'babel-regenerator-runtime' : null,
+      './src'
+    ].filter(Boolean),
+    output: {
+      path: path.resolve(outDir),
+      filename: outFile,
+      libraryTarget: 'umd',
+      library: 'Fetchers',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        target === 'es5' ? getBabelLoader() : null
+      ].filter(Boolean)
+    },
+    plugins: [
+      new webpack.BannerPlugin(`${packageJson.name} v${packageJson.version}`)
     ]
-  },
-  plugins: [
-    new webpack.BannerPlugin(`${packageJson.name} v${packageJson.version}`)
-  ]
-};
+  }
+}
+
+function getBabelLoader() {
+  return {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['env'],
+      }
+    }
+  };
+}
